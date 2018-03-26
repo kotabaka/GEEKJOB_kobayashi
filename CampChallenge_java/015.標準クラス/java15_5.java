090d71df8d0fbacbea52d21e1f222b1c0f2a4035
EXP-0013\guest1Day <kotabakayatashini@gmail.com>
2018-03-26 19:24:15 +0900

    java課題標準クラス15-5提出します

---
+++ CampChallenge_java/015.標準クラス/java15_5.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java15_moji;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class java15_5 {
+ public static void main(String[]args){
+     String name ="こばやしけんと";
+     System.out.println(name.length());
+     System.out.println(name.getBytes().length);
+ }   
+}
