c8840e0087cc9ed847e04004c8c76f2693ea1ad3
EXP-0013\guest1Day <kotabakayatashini@gmail.com>
2018-03-30 13:16:35 +0900

    java課題データ操作16-5を提出します

---
+++ CampChallenge_java/016.データ操作/java16_5.java
@@ -0,0 +1,113 @@
+package java16;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import javax.servlet.http.Cookie;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(urlPatterns = {"/java16_5"})
+public class java16_5 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet java16_5</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //日時を取得
+            Date today =new Date();
+            
+            //todayをStringに変換
+            String s =String.valueOf(today);
+            
+            //Cookieに日時を入れる
+            Cookie c =new Cookie("Data",s);
+            response.addCookie(c);
+            
+            Cookie[] cs = request.getCookies();
+            if (cs != null) {
+                for (int i = 0; i < cs.length; i++) {
+                    String CookieName = cs[i].getName();
+                    if (CookieName.equals("Data")) {
+                        String CookieValue = cs[i].getValue();
+                        out.print("最後のログインは、" + CookieValue);
+                        break;
+                    }
+                }
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
