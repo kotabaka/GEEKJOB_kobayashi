89ef7b25c57fac2f60d9f029f3be83309f4258c0
EXP-0013\guest1Day <kotabakayatashini@gmail.com>
2018-04-04 17:02:08 +0900

    java課題DB操作18-2を提出します

---
+++ CampChallenge_java/018.DB操作/java18_2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java18;
+import java.sql.*;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class java18_2 {
+     public static void main(String[] args){
+        Connection db_con =null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","KOBAYASHIKENTO","3649");
+            System.out.println("接続できました");
+            System.out.println("GetCatalog:"+db_con.getCatalog());
+            
+            Statement stmt=db_con.createStatement();
+            String sql ="insert into profiles(profilesID,name,tel,age,birthday) values (6,'小林　健人','090-1234-9876',27,'1990-08-30')";
+            int num =stmt.executeUpdate(sql);
+            sql="select * from profiles";
+            
+            ResultSet rs =stmt.executeQuery(sql);
+            while(rs.next()){
+                int id = rs.getInt("profilesID");
+                String name =rs.getString("name");
+                String tel =rs.getString("tel");
+                int age =rs.getInt("age");
+                Date date =rs.getDate("birthday");
+                
+                System.out.println(id+name+tel+age+date);
+                
+                
+            }
+            rs.close();
+                stmt.close();
+            
+        }catch(Exception e_con){
+            System.out.println(e_con.getMessage());
+        }
+        
+    }
+}
